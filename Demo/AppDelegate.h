d31c82c63e3d33c64a3d4af10dc24f39eb5802f2
CK GuoZi <ckguozi@ckdemacbook-pro.local>
2017-09-09 14:00:53 +0800

    Initial Commit

---
+++ Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by CK GuoZi on 2017/9/9.
+//  Copyright © 2017年 CK GuoZi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
